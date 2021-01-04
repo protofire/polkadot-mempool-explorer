14868368398e4c658f7ae6e45f5d93996b5bbe0a
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2021-01-04 16:07:40 -0300

    (Add) main page and router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Redirect, Route, HashRouter as Router, Switch } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { Footer, Header } from 'components/layout'
+import { BaseCard } from 'components/pureStyledComponents/BaseCard'
+import { CardSubtitle } from 'components/pureStyledComponents/CardSubtitle'
+import { CardText } from 'components/pureStyledComponents/CardText'
 import { InnerContainer } from 'components/pureStyledComponents/InnerContainer'
 import { MainScroll } from 'components/pureStyledComponents/MainScroll'
 import { MainWrapper } from 'components/pureStyledComponents/MainWrapper'
+import { Main } from 'pages/Main'
 import theme from 'theme'
 import { GlobalStyle } from 'theme/globalStyle'
 
@@ -16,7 +21,22 @@ export const App: React.FC = () => (
     <MainWrapper>
       <Header />
       <MainScroll>
-        <InnerContainer>Stuff</InnerContainer>
+        <InnerContainer>
+          <Router>
+            <Switch>
+              <Route component={Main} exact path="/main" />
+              <Route exact path="/">
+                <Redirect to="/main" />
+              </Route>
+              <Route path="*">
+                <BaseCard>
+                  <CardSubtitle>Error 404</CardSubtitle>
+                  <CardText>Page not found...</CardText>
+                </BaseCard>
+              </Route>
+            </Switch>
+          </Router>
+        </InnerContainer>
         <Footer />
       </MainScroll>
     </MainWrapper>
