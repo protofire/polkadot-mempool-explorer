057c0015eb568ae6edf81839bdef57fee49940ac
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2021-01-04 17:12:45 -0300

    (add) main page dropdown

--- web/src/pages/Main/index.tsx
+++ web/src/pages/Main/index.tsx
@@ -1,14 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { Dropdown, DropdownItem, DropdownPosition } from 'components/common/Dropdown'
 import { SearchField } from 'components/form/SearchField'
+import { ChevronDown } from 'components/icons/ChevronDown'
 import { PageTitle } from 'components/text/PageTitle'
 
 const Wrapper = styled.div``
 
+const TitleDropdownButton = styled.button`
+  align-items: center;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  outline: none;
+  padding: 0;
+`
+
+const TitleDropdownButtonText = styled.span`
+  color: ${(props) => props.theme.colors.mediumGrey};
+  font-size: 15px;
+  font-weight: 500;
+
+  .isOpen & {
+    color: ${(props) => props.theme.colors.textColor};
+  }
+`
+
+const ChevronDownStyled = styled(ChevronDown)`
+  flex-shrink: 0;
+  margin-left: 12px;
+
+  .fill {
+    stroke: ${(props) => props.theme.colors.mediumGrey};
+  }
+
+  .isOpen & {
+    .fill {
+      stroke: ${(props) => props.theme.colors.textColor};
+    }
+  }
+`
+
 export const Main: React.FC = (props) => {
   const { ...restProps } = props
-  const dropdownItems = [
+  const searchDropdownItems = [
     {
       onClick: () => {
         console.log('All')
@@ -46,11 +84,32 @@ export const Main: React.FC = (props) => {
     },
   ]
   const [searchValue, setSearchValue] = React.useState('')
+  const titleDropdownItems = [
+    {
+      onClick: () => {
+        console.log('All Transactions')
+      },
+      text: 'All Transactions',
+    },
+    {
+      onClick: () => {
+        console.log('In Mempool')
+      },
+      text: 'In Mempool',
+    },
+    {
+      onClick: () => {
+        console.log('Just Removed')
+      },
+      text: 'Just Removed',
+    },
+  ]
+  const [currentTitleDropdownItems, setCurrentTitleDropdownItems] = React.useState(0)
 
   return (
     <Wrapper {...restProps}>
       <SearchField
-        dropdownItems={dropdownItems}
+        dropdownItems={searchDropdownItems}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
           setSearchValue(e.currentTarget.value)
         }}
@@ -59,7 +118,36 @@ export const Main: React.FC = (props) => {
         }}
         value={searchValue}
       />
-      <PageTitle extraControls={'asdasd'}>Mempool Explorer</PageTitle>
+      <PageTitle
+        extraControls={
+          <Dropdown
+            activeItemHighlight={true}
+            currentItem={currentTitleDropdownItems}
+            dropdownButtonContent={
+              <TitleDropdownButton>
+                <TitleDropdownButtonText>
+                  {titleDropdownItems[currentTitleDropdownItems].text}
+                </TitleDropdownButtonText>
+                <ChevronDownStyled />
+              </TitleDropdownButton>
+            }
+            dropdownPosition={DropdownPosition.right}
+            items={titleDropdownItems.map((item, index) => (
+              <DropdownItem
+                key={index}
+                onClick={() => {
+                  item.onClick()
+                  setCurrentTitleDropdownItems(index)
+                }}
+              >
+                {item.text}
+              </DropdownItem>
+            ))}
+          />
+        }
+      >
+        Mempool Explorer
+      </PageTitle>
     </Wrapper>
   )
 }
